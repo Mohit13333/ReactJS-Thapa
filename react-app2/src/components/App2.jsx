f8f7c9a058c1e62665e4f7a8db9c04bf6ae64f0e
Mohit13333 <mohitkumar143680@gmail.com>
2024-09-28 16:28:35 +0530

    dynamic value

--- react-app2/src/components/App2.jsx
+++ react-app2/src/components/App2.jsx
@@ -1,18 +1,25 @@
 function App2() {
-    return (
-      <>
-        <div>
-          <img src="qot.jpeg" alt="qot" width="40%" height="40%" />
-        </div>
-        <h2>Name: Queens of tears</h2>
-        <h3>Rating:8.2</h3>
-        <p>
-          Summary: Beak Hyun-woo and Hong Hae-in navigate a tense relationship,
-          both at home and at work. But upon deciding his future, Hyun-woo pays a
-          visit to his family.
-        </p>
-      </>
-  )
+  const name = "Queens of tears";
+  const rating = "8.2";
+  const summary =
+    "Beak Hyun-woo and Hong Hae-in navigate a tense relationship,both at home and at work. But upon deciding his future, Hyun-woo pays avisit to his family.";
+
+  // function calls
+  const returnGenre = () => {
+    const Genre = "RomCom";
+    return Genre;
+  };
+  return (
+    <>
+      <div>
+        <img src="qot.jpeg" alt="qot" width="40%" height="40%" />
+      </div>
+      <h2>Name: {name}</h2>
+      <h3>Rating:{55 / 11}</h3>
+      <p>Summary: {summary}</p>
+      <p>Genre: {returnGenre()}</p>
+    </>
+  );
 }
 
-export default App2
+export default App2;
