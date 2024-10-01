68d6ae4175cdf10381429b8ad427e13be88f5f38
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-01 12:31:45 +0530

    tailwind config

--- react-app2/src/components/App2.jsx
+++ react-app2/src/components/App2.jsx
@@ -3,7 +3,8 @@ import SeriesCard from "./SeriesCard"
 function App2() {
   return (
     <>
-      <ul>
+    <h1 className="font-bold text-center mb-2">List of netflix series</h1>
+      <ul className="grid grid-cols-3 gap-6 m-2">
         {seriesData.map((curelem)=>{
           return(
        <SeriesCard key={curelem.id} curelem={curelem}/>
--- react-app2/src/components/SeriesCard.jsx
+++ react-app2/src/components/SeriesCard.jsx
@@ -1,21 +1,22 @@
-const SeriesCard=(props)=>{
+// const SeriesCard=(props)=>{
+const SeriesCard=({curelem})=>{
+  // console.log(props)
+  const {id,img_url,name,rating,description,genre,cast,watch_url} = curelem;
     return (
-        <li key={props.curelem.id}>
-          <div>
+        <li className="shadow-md">
+          <div className="width=100%">
             <img
-              src={props.curelem.img_url}
-              alt={props.curelem.id}
-              width="40%"
-              height="40%"
+              src={img_url}
+              alt={id}
             />
           </div>
-          <h2>Name: {props.curelem.name}</h2>
-          <h3>Rating:{props.curelem.rating}</h3>
-          <p>Summary: {props.curelem.description}</p>
-          <p>Genre: {props.curelem.genre}</p>
-          <p>Cast: {props.curelem.cast}</p>
-          <a href={props.curelem.watch_url} target="_blank">
-            <button>Watch Now</button>
+          <h2 className="text-lg font-bold">Name: {name}</h2>
+          <h3 className="text-md font-bold">Rating: <span className={` p-1 text-black rounded-[100rem] ${rating>=8.5?"bg-green-300":"bg-yellow-300"}`}>{rating}</span></h3>
+          <p className="mt-2">Summary: {description}</p>
+          <p className="mt-2">Genre: {genre}</p>
+          <p className="mt-2">Cast: {cast}</p>
+          <a href={watch_url} target="_blank">
+            <button className={` p-1 font-bold mt-4 text-black  ${rating>=8.5?"bg-green-300":"bg-yellow-300"}`}>Watch Now</button>
           </a>
         </li>
     )
