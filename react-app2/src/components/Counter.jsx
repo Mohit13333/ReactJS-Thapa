d9e897a1190e05e48862e01a00f02d42befb049d
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-04 22:48:20 +0530

    counter fix

--- react-app2/src/components/Counter.jsx
+++ react-app2/src/components/Counter.jsx
@@ -1,17 +1,24 @@
 import React,{useState} from 'react';
 
 const Counter=()=>{
-    const [count, setCount] = useState(-20);
+    const [count, setCount] = useState(0);
         const handleButtonClick=()=>{
             // count++;
             // setCount(count)
             setCount(()=>count+1)
-            console.log(count);
+            console.log(count+1);
+        };
+        const handleDecrementButton=()=>{
+            setCount(()=>count==0?0:count-1)
         }
-    return (
+        return (
         <>
         <h1 className='bg-slate-900 text-white rounded-lg p-5 text-center w-[100px]'>{count}</h1>
+        <br/>
         <button className='bg-black text-white rounded-lg p-5' onClick={handleButtonClick}>Increment</button>
+        <br />
+        <br />
+        <button className='bg-black text-white rounded-lg p-5' onClick={handleDecrementButton}>Decrement</button>
         </>
     )
 }
