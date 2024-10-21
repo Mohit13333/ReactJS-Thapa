dd270c09f614c54aeb3d631f0ba5783ec9e93217
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-21 15:27:39 +0530

    inputcounter

--- Shor-Circuit-Eval/src/App.jsx
+++ Shor-Circuit-Eval/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ShortCircuit from './components/ShortCircuit'
 import Index from './components/UseState'
+import Chalange from './components/UseState/Chalange'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
+   <Chalange></Chalange>
    <ShortCircuit/>
    <Index/>
    </>
---
+++ Shor-Circuit-Eval/src/components/UseState/Chalange.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Chalange = () => {
+    const [count,setCount]=useState(0);
+    const [step,setStep] = useState(0);
+    const handleIncrement=() =>{
+        setCount(count+step);
+        // count<100?setCount(count+step):count;
+    }
+    const handleDecrement=() =>{
+        setCount(count-step);
+        // count <= 0 ? 0: setCount(count-step)
+    }
+  return (
+    <div>
+      <h1>UsesState Chalange</h1>
+      <p>Count: {count}</p>
+      <label>
+      Step:  
+      <input type="number" value={step} onChange={(e)=>setStep(Number(e.target.value))}></input>
+      </label>
+      <br/>
+      <br/>
+      <button onClick={handleIncrement} disabled={count>=100}>Increment</button>
+      <button onClick={handleDecrement} disabled={count<0}>Decrement</button>
+      <button onClick={()=>setCount(0)}>Reset</button>
+    </div>
+  )
+}
+
+export default Chalange
