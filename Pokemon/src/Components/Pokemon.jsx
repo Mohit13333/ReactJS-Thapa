296ebab6fe00ffd12eefe5e3c7bdaee629b1aa92
Mohit13333 <mohitkumar143680@gmail.com>
2024-10-29 22:01:16 +0530

    push

--- Pokemon/src/Components/Pokemon.jsx
+++ Pokemon/src/Components/Pokemon.jsx
@@ -15,7 +15,7 @@ const Pokemon = () => {
       const detailedData = await Promise.all(
         response.data.results.map(async (curPokemon) => {
           const result = await axios.get(curPokemon.url);
-          return result.data; 
+          return result.data;
         })
       );
       setPokemon(detailedData);
@@ -31,34 +31,33 @@ const Pokemon = () => {
     getPokemonData();
   }, []);
 
+  // const fetchPokemon = async () => {
+  //     try {
+  //       const res = await fetch(API);
+  //       const data = await res.json();
+  //       //   console.log(data);
 
-// const fetchPokemon = async () => {
-//     try {
-//       const res = await fetch(API);
-//       const data = await res.json();
-//       //   console.log(data);
+  //       const detailedPokemonData = data.results.map(async (curPokemon) => {
+  //         const res = await fetch(curPokemon.url);
+  //         const data = await res.json();
+  //         return data;
+  //       });
+  //       //   console.log(detailedPokemonData);
 
-//       const detailedPokemonData = data.results.map(async (curPokemon) => {
-//         const res = await fetch(curPokemon.url);
-//         const data = await res.json();
-//         return data;
-//       });
-//       //   console.log(detailedPokemonData);
+  //       const detailedResponses = await Promise.all(detailedPokemonData);
+  //       console.log(detailedResponses);
+  //       setPokemon(detailedResponses);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //       setLoading(false);
+  //       setError(error);
+  //     }
+  //   };
 
-//       const detailedResponses = await Promise.all(detailedPokemonData);
-//       console.log(detailedResponses);
-//       setPokemon(detailedResponses);
-//       setLoading(false);
-//     } catch (error) {
-//       console.log(error);
-//       setLoading(false);
-//       setError(error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchPokemon();
-//   }, []);
+  //   useEffect(() => {
+  //     fetchPokemon();
+  //   }, []);
 
   const searchData = pokemon.filter((curPokemon) =>
     curPokemon.name.toLowerCase().includes(search.toLowerCase())
